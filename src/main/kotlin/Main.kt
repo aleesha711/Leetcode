18f59e5804319e0256f35552b678da2438800fda
aleeshakanwal <aleesha_kanwal@yahoo.com>
2022-05-15 12:06:47 +0200

    Two Sum

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,8 @@ fun main(args: Array<String>) {
     println(containsDuplicate2(array).toString())
     println("isAnagram1 " + isAnagram1("cat","tac").toString())
     println("isAnagram2 " + isAnagram2("anagram","nagaram").toString())
+    val stringArray =  twoSum2(array,4).map { it.toString() }.toTypedArray()
+    println("TwoSum1" + stringArray.contentToString())
 }
 
 //1. Easy -> Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
@@ -64,4 +66,44 @@ fun isAnagram2(s: String, t: String): Boolean {
 
     return true
 }
+
+
+//<------------------------------------------------------------------------------------------------------->
+
+//3. Easy -> Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//Solution 1
+fun twoSum1(nums: IntArray, target: Int): IntArray {
+
+    var complimentHashmap = HashMap<Int, Int>()
+
+    for(i in nums.indices) {
+        var compliment =  target - nums.get(i)
+        if(complimentHashmap.contains(compliment))
+            return intArrayOf(complimentHashmap.get(compliment)!!, i)
+
+        complimentHashmap.put(nums.get(i), i)
+    }
+
+    return intArrayOf(0)
+
+}
+
+//Solution 2
+fun twoSum2(nums: IntArray, target: Int): IntArray {
+
+    if(nums.isEmpty())
+        return intArrayOf(0)
+
+    for(i in nums.indices)
+        for(j in i+1 until nums.size) {
+            if(nums[i] + nums[j] == target) {
+                return intArrayOf(i,j)
+            }
+        }
+
+    return intArrayOf(0)
+
+}
+
+
 
