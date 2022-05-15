fb4032e11f2d82baae91b94fb1627b00b5e79890
aleeshakanwal <aleesha_kanwal@yahoo.com>
2022-05-15 18:26:50 +0200

    Group Anagrams

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+import kotlin.collections.HashMap
+import kotlin.collections.HashSet
+
 fun main(args: Array<String>) {
     println("Welcome")
     val array = intArrayOf(1,2,3,4,5,6)
@@ -7,6 +10,10 @@ fun main(args: Array<String>) {
     println("isAnagram2 " + isAnagram2("anagram","nagaram").toString())
     val stringArray =  twoSum2(array,4).map { it.toString() }.toTypedArray()
     println("TwoSum1" + stringArray.contentToString())
+    intersection2(array, array)
+    val strs = arrayOf("eat","tea","tan","ate","nat","bat")
+    groupAnagrams1(strs)
+    groupAnagrams2(strs)
 }
 
 //1. Easy -> Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
@@ -67,7 +74,6 @@ fun isAnagram2(s: String, t: String): Boolean {
     return true
 }
 
-
 //<------------------------------------------------------------------------------------------------------->
 
 //3. Easy -> Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
@@ -102,8 +108,71 @@ fun twoSum2(nums: IntArray, target: Int): IntArray {
         }
 
     return intArrayOf(0)
-
 }
 
+//<------------------------------------------------------------------------------------------------------->
+//4. Easy --> return an array of intersection
+fun intersection1(nums1: IntArray, nums2: IntArray): IntArray {
+    var result = intArrayOf()
+    for(i in nums1.indices) {
+        for(j in nums2.indices) {
+            if(nums1[i] == nums2[j])
+                result[nums1[i]]
+        }
+    }
 
+    return result
+}
 
+fun intersection2(nums1: IntArray, nums2: IntArray): IntArray {
+
+    val set1= nums1.toSet()
+    val set2= nums2.toSet()
+    val result = set1.intersect(set2)
+    return result.toIntArray()
+}
+//<------------------------------------------------------------------------------------------------------->
+//4. Medium --> Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+fun groupAnagrams1(strs: Array<String>): List<List<String>> {
+
+    var hashmap = hashMapOf<String, List<String>>()
+
+    for(i in strs.indices) {
+
+        val key = countAscii(strs.get(i))
+        if(hashmap.containsKey(key))
+            {
+                var ar = hashmap.get(key)!!.toMutableList()
+                ar.add(strs[i])
+
+                hashmap.put(key, ar)
+            }
+
+        else
+        {
+            hashmap.put(key, listOf(strs[i]))
+        }
+
+    }
+
+    return hashmap.values.toList()
+}
+
+fun countAscii(name: String): String {
+    val count = IntArray(26)
+
+    val nameArray = name.toCharArray()
+
+    for(i in nameArray.indices) {
+        count[nameArray[i] - 'a']++
+    }
+
+    count.contentToString()
+    return count.contentToString()
+
+}
+//Solution 2
+fun groupAnagrams2(strs: Array<String>): List<List<String>> {
+    return strs.groupBy { it.toCharArray().sorted().toString() }.values.toList()
+}
